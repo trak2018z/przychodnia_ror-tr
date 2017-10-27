94dfdc9e0937fe23cc0fa2549034d2f12a8b4e88
hobosoccer <piotrknott@interia.eu>
2017-10-27 12:20:24 +0200

    Dodanie stylu Bootstrap do wiadomości flash oraz polskich tłumaczeń I18n

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025205211) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "clinics", force: :cascade do |t|
-    t.string "description"
-    t.string "hours"
-    t.string "address"
-    t.string "telephone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
